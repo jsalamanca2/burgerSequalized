59eee4433ae82bef8af708e22b3801d6b61e9495
jsalamanca11 <j.salamanca611@gmail.com>
2017-02-09 15:13:54 -0500

    took out orm.js and sequalized connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,15 @@
-var mysql = require("mysql")
+var Sequelize = require("sequelize");
 
+// Creates mySQL connection
+var sequelize = new Sequelize("burger_db", "root", "password", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
-var connection = mysql.createConnection({
-			host:"localhost",
-			port: 3306,
-			user: "root",
-			password:"password",
-			database:"burgers_db"
-	})
-
-connection.connect(function(err) {
-	if (err) throw err;
-	console.log("connected as id " + connection.threadId);
-})
-
-//exports connection to be used in orm.js file
-module.exports= connection;
+// Exports the connection
+module.exports = sequelize;
